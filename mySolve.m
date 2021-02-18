80e111cc52a86539279e128f058a41784d8cb00e
knarla15 <45083716+knarla15@users.noreply.github.com>
2021-02-18 18:26:06 -0500

    Update mySolve.m

--- mySolve.m
+++ mySolve.m
@@ -1,23 +1,15 @@
-%Branavan Kalapathy
-%MATH344L 003
-%Lab 3
-% m-file that solves A*x = b
+% Kundana Narla
+% Math 344L, Lab 4
+% mySolve.m
+% basically using backward.m and forward.m functions in one file
+
+%declaring the function
 function x = mySolve(A,b) % solve function
-    if (size(A,1) ~= size(A,2)) % check to see if given matrix is square
-        error('Matrix A is not a square matrix');
-        elseif (size(b,1) == 1 && size(b,2)~=1) % column vector checkl
-            error('vector b is not a column vector');
-    else 
-        
-            
-        [L,U] = MYLU(A); %MYLU Factorization of matrix A
-        y = forward(L,b); %Calls forward
-      if (size(A,1) ~= size(b,1)) %dimensions check
-        error('invalid dimensions, n # of columns of A doees not match n # of columns of b');
-      else
-          
-        x = backward(U,y); %calls backward
-      end 
+    if (size(A,1) == size(A,2)       
+        [L,U] = MYLU(A); //lu
+        y = forward(L,b);
+    if (size(A,1) == size(b,1))
+        x = backward(U,y);
+        end 
     end
 end
-
